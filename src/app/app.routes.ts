dabee47b89c06d717d6a1986888bf99136bb3370
Hortense Cauchois <hortensecauchois@gmail.com>
2025-01-17 23:23:39 +0100

    feat: add add-route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {AddCategoryComponent} from './add-category/add-category.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'add', component: AddCategoryComponent}
+];
