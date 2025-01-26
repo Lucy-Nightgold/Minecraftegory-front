9c652cc1ae3b7faef835cdd1f314d5524f35f237
Hortense Cauchois <hortensecauchois@gmail.com>
2025-01-26 15:30:49 +0100

    feat: add update menu

--- src/app/add-category/add-category.component.ts
+++ src/app/add-category/add-category.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {Category} from '../entities/category.entity';
 import {CategoryService} from '../services/category.service';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-add-category',
@@ -14,7 +14,7 @@ import {Router} from '@angular/router';
   styleUrl: './add-category.component.scss'
 })
 export class AddCategoryComponent {
-  catgeoryForm: FormGroup = new FormGroup({
+  categoryForm: FormGroup = new FormGroup({
     name: new FormControl("", Validators.compose(
       [Validators.required,
       Validators.maxLength(50),
@@ -23,23 +23,43 @@ export class AddCategoryComponent {
     parentId: new FormControl("", Validators.required)
   })
 
+  id: number = 0;
+
   potentialParents: Category[] = [];
 
-  constructor(private service: CategoryService, private router: Router) {}
+  constructor(private service: CategoryService, private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.service.getAllCategories().subscribe(categories => {
-      this.potentialParents = categories;
+    this.route.params.subscribe(params => {
+      this.id = params['id'] == undefined ? 0: +params['id'];
+      if (this.id != 0) {
+        this.service.getCategory(this.id).subscribe(category => {
+          this.categoryForm.controls['name'].setValue(category.name);
+          this.categoryForm.controls['parentId'].setValue(category.parentId);
+          this.service.getAvailableParents(category.id).subscribe(categories => {
+            this.potentialParents = categories;
+          })
+        })
+      } else {
+        this.service.getAllCategories().subscribe(categories => {
+          this.potentialParents = categories;
+        })
+      }
     })
   }
 
   onSubmit() {
-    if (this.catgeoryForm.invalid) {
-      console.log(this.catgeoryForm.controls['name'].valid + ":" + this.catgeoryForm.controls['parentId'].valid);
+    if (this.categoryForm.invalid) {
       return;
     }
-    this.service.sendCategory(this.catgeoryForm.controls['name'].value, this.catgeoryForm.controls['parentId'].value).subscribe(() => {
-      this.router.navigate(['']);
-    })
+    if (this.id == 0) {
+      this.service.sendCategory(this.categoryForm.controls['name'].value, this.categoryForm.controls['parentId'].value).subscribe(() => {
+        this.router.navigate(['']);
+      });
+    } else {
+      this.service.updateCategory(this.id, this.categoryForm.controls['name'].value, this.categoryForm.controls['parentId'].value).subscribe(() => {
+        this.router.navigate(['']);
+      })
+    }
   }
 }
